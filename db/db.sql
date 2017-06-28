0a4620ea4bb4be3ad1ddc31e4e1e708ff2125f25
Ajay Balasa <ajaybalasa@Ajays-MacBook-Pro.local>
2017-06-27 23:17:42 -0700

    Added db related files

---
+++ db/db.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `guid` varchar(255) DEFAULT NULL,
+  `creation_date` datetime DEFAULT NULL,
+  `is_provider` tinyint(1) DEFAULT '0',
+  `is_active` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `guid` (`guid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
