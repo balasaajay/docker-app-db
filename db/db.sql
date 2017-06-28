36b024b0ef22bf4e4dab7fed840929f86c7a490f
Ajay Balasa <ajaybalasa@Ajays-MacBook-Pro.local>
2017-06-27 23:23:54 -0700

    Adding data into mysqldb

--- db/db.sql
+++ db/db.sql
@@ -11,3 +11,9 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 GRANT ALL PRIVILEGES ON test.* TO 'root'@'%' IDENTIFIED BY 'test';
+
+INSERT INTO user values (1,'test_user1',NULL,1,0);
+INSERT INTO user values (2,'test_user2',NULL,0,1);
+INSERT INTO user values (3,'test_user3',NULL,0,0);
+INSERT INTO user values (4,'test_user4',NULL,0,1);
+INSERT INTO user values (5,'test_user5',NULL,1,1);
